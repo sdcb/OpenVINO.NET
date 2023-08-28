0d5081d552cf795dd52d8b9368ce2c2dd70c8063
sdcb <sdflysha@qq.com>
2023-08-28 22:47:20 +0800

    first workable nuget version.

---
+++ tests/Sdcb.OpenVINO.Tests/Natives/VersionTest.cs
@@ -0,0 +1,26 @@
+﻿using Sdcb.OpenVINO.Natives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Sdcb.OpenVINO.Tests.Natives;
+
+public class VersionTest
+{
+    private readonly ITestOutputHelper _console;
+
+    public VersionTest(ITestOutputHelper console)
+    {
+        _console = console;
+    }
+
+    [Fact]
+    public unsafe void PrintVersion()
+    {
+        ov_version v = new();
+        NativeMethods.ov_get_openvino_version(&v);
+    }
+}
