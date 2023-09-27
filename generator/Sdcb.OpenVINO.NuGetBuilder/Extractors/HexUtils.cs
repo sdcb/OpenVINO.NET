2777771788b66f83b86b8f1e5cc5477ab79aab77
sdcb <sdflysha@qq.com>
2023-09-27 12:24:59 +0800

    minor fix

--- generator/Sdcb.OpenVINO.NuGetBuilder/Extractors/HexUtils.cs
+++ generator/Sdcb.OpenVINO.NuGetBuilder/Extractors/HexUtils.cs
@@ -1,30 +1,28 @@
 ﻿using System.Globalization;
 using System.Text;
 
-namespace Sdcb.OpenVINO.NuGetBuilders.Extractors
+namespace Sdcb.OpenVINO.NuGetBuilders.Extractors;
+
+internal static class HexUtils
 {
-    internal static class HexUtils
+    internal static string ByteArrayToHexString(byte[] byteArray)
     {
-
-        internal static string ByteArrayToHexString(byte[] byteArray)
+        StringBuilder hex = new(byteArray.Length * 2);
+        foreach (byte b in byteArray)
         {
-            StringBuilder hex = new(byteArray.Length * 2);
-            foreach (byte b in byteArray)
-            {
-                hex.AppendFormat("{0:x2}", b);
-            }
-            return hex.ToString();
+            hex.AppendFormat("{0:x2}", b);
         }
+        return hex.ToString();
+    }
 
-        internal static byte[] HexStringToByteArray(string hexString)
+    internal static byte[] HexStringToByteArray(string hexString)
+    {
+        byte[] byteArray = new byte[hexString.Length / 2];
+        for (int i = 0; i < byteArray.Length; i++)
         {
-            byte[] byteArray = new byte[hexString.Length / 2];
-            for (int i = 0; i < byteArray.Length; i++)
-            {
-                string hex = hexString[(i * 2)..(i * 2 + 2)];
-                byteArray[i] = byte.Parse(hex, NumberStyles.HexNumber);
-            }
-            return byteArray;
+            string hex = hexString[(i * 2)..(i * 2 + 2)];
+            byteArray[i] = byte.Parse(hex, NumberStyles.HexNumber);
         }
+        return byteArray;
     }
 }
