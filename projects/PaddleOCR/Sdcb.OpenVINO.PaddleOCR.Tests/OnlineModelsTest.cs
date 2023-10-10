a0b9bbf170131ba5b2678dad0303fd0824ec1f4c
sdcb <sdflysha@qq.com>
2023-10-10 12:33:10 +0800

    add v4 fast check ocr

--- projects/PaddleOCR/Sdcb.OpenVINO.PaddleOCR.Tests/OnlineModelsTest.cs
+++ projects/PaddleOCR/Sdcb.OpenVINO.PaddleOCR.Tests/OnlineModelsTest.cs
@@ -1,8 +1,8 @@
 using OpenCvSharp;
 using Sdcb.OpenVINO.PaddleOCR.Models.Online;
 using Sdcb.OpenVINO.PaddleOCR.Models;
-using System;
 using Xunit.Abstractions;
+using System.Diagnostics;
 
 namespace Sdcb.OpenVINO.PaddleOCR.Tests;
 
@@ -33,12 +33,44 @@ public class OnlineModelsTest
             // using (Mat src2 = Cv2.ImRead(@"C:\test.jpg"))
             using (Mat src = Cv2.ImDecode(sampleImageData, ImreadModes.Color))
             {
+                Stopwatch sw = Stopwatch.StartNew();
                 PaddleOcrResult result = all.Run(src);
+                _console.WriteLine($"elapsed={sw.ElapsedMilliseconds} ms");
                 _console.WriteLine("Detected all texts: \n" + result.Text);
                 foreach (PaddleOcrResultRegion region in result.Regions)
                 {
                     _console.WriteLine($"Text: {region.Text}, Score: {region.Score}, RectCenter: {region.Rect.Center}, RectSize:    {region.Rect.Size}, Angle: {region.Rect.Angle}");
                 }
+            }
+        }
+    }
+
+    [Fact]
+    public async Task V4FastCheckOCR()
+    {
+        OnlineFullModels onlineModels = new OnlineFullModels(
+            OnlineDetectionModel.ChineseV4, OnlineClassificationModel.ChineseMobileV2, LocalDictOnlineRecognizationModel.EnglishV4);
+        FullOcrModel model = await onlineModels.DownloadAsync();
+
+        // from: https://visualstudio.microsoft.com/wp-content/uploads/2021/11/Home-page-extension-visual-updated.png
+        byte[] sampleImageData = File.ReadAllBytes(@"./samples/vsext.png");
+
+        using (PaddleOcrAll all = new(model)
+        {
+            AllowRotateDetection = true,
+            Enable180Classification = false,
+        })
+        {
+            // Load local file by following code:
+            // using (Mat src2 = Cv2.ImRead(@"C:\test.jpg"))
+            using (Mat src = Cv2.ImDecode(sampleImageData, ImreadModes.Color))
+            {
+                PaddleOcrResult result = null!;
+                Stopwatch sw = Stopwatch.StartNew();
+                result = all.Run(src);
+
+                _console.WriteLine($"elapsed={sw.ElapsedMilliseconds} ms");
+                _console.WriteLine("Detected all texts: \n" + result.Text);
             }
         }
     }
