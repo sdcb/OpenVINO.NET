0585d4beab24d0fd0243c4a9a6b8fed591cad837
sdcb <sdflysha@qq.com>
2023-10-19 22:15:46 +0800

    Add cls rotation test.

---
+++ projects/PaddleOCR/Sdcb.OpenVINO.PaddleOCR.Tests/ClsTest.cs
@@ -0,0 +1,20 @@
+﻿using OpenCvSharp;
+using Sdcb.OpenVINO.PaddleOCR.Models.Online;
+
+namespace Sdcb.OpenVINO.PaddleOCR.Tests;
+
+public class ClsTest
+{
+    [Fact]
+    public async Task ClsResultIsCorrect()
+    {
+        using Mat src = Cv2.ImRead("./samples/5ghz.jpg");
+        using PaddleOcrClassifier cls = new(await OnlineClassificationModel.ChineseMobileV2.DownloadAsync());
+        bool shouldRotate = cls.ShouldRotate180(src);
+        Assert.False(shouldRotate);
+
+        Cv2.Rotate(src, src, RotateFlags.Rotate180);
+        bool shouldRotate2 = cls.ShouldRotate180(src);
+        Assert.True(shouldRotate2);
+    }
+}
