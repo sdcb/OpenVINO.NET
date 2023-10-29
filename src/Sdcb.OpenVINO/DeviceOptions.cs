ccc0f42e01ab5a09256e9de785e1e6448958ea65
sdcb <sdflysha@qq.com>
2023-10-29 12:42:46 +0800

    Add NumStreams.

--- src/Sdcb.OpenVINO/DeviceOptions.cs
+++ src/Sdcb.OpenVINO/DeviceOptions.cs
@@ -65,6 +65,25 @@ public record DeviceOptions
     }
 
     /// <summary>
+    /// Gets or sets the number of inference streams.
+    /// </summary>
+    public NumStreamDef? NumStreams
+    {
+        get => Properties.TryGetValue(PropertyKeys.NumStreams, out string? val) ? NumStreamDef.Parse(val) : null;
+        set
+        {
+            if (value != null)
+            {
+                Properties[PropertyKeys.NumStreams] = value.Value.ToString();
+            }
+            else
+            {
+                Properties.Remove(PropertyKeys.NumStreams);
+            }
+        }
+    }
+
+    /// <summary>
     /// Gets or sets the inference performance mode. 
     /// </summary>
     public PerformanceMode? PerformanceMode
@@ -103,4 +122,29 @@ public record DeviceOptions
     /// The default device name to be AUTO.
     /// </summary>
     public const string DefaultDeviceName = "AUTO";
+}
+
+public readonly record struct NumStreamDef(int StreamCount)
+{
+    public static NumStreamDef Auto => new(-1);
+    public static NumStreamDef Numa => new(-2);
+    public static implicit operator NumStreamDef(int streamCount) => streamCount switch
+    {
+        < -2 => throw new ArgumentException("value must >= -2 or AUTO/NUMA", nameof(streamCount)),
+        _ => new NumStreamDef(streamCount),
+    };
+
+    public static NumStreamDef Parse(string streamCount) => streamCount switch
+    {
+        "AUTO" => Auto,
+        "NUMA" => Numa,
+        _ => int.Parse(streamCount)
+    };
+
+    public override string ToString() => StreamCount switch
+    {
+        -1 => "AUTO",
+        -2 => "NUMA",
+        _ => StreamCount.ToString()
+    };
 }
