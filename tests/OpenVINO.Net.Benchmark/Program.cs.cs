45f5ae299041513ca2d0df3b911749731a1c2898
Aven <aven@ocr.one>
2023-11-22 15:34:20 +0800

    initiate benchmark project

---
+++ tests/OpenVINO.Net.Benchmark/Program.cs.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenVINO.Net.Benchmark
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
