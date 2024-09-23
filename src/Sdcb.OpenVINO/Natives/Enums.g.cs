a64c35dc373fd3594c4c2c7ff55ac188369a2dad
sdcb <sdflysha@qq.com>
2024-09-23 12:53:40 +0800

    fix compilation error

--- src/Sdcb.OpenVINO/Natives/Enums.g.cs
+++ src/Sdcb.OpenVINO/Natives/Enums.g.cs
@@ -69,7 +69,7 @@ public enum ov_status_e
 /// <para>This enum contains codes for element type, which is aligned with ov::element::Type_t in</para>
 /// <para>src/core/include/openvino/core/type/element_type.hpp</para>
 /// </summary>
-[CSourceInfo("ov_common.h", 171, 197, "ov_base_c_api")]
+[CSourceInfo("ov_common.h", 171, 199, "ov_base_c_api")]
 public enum ov_element_type_e
 {
     /// <summary>Undefined element type</summary>
@@ -145,6 +145,12 @@ public enum ov_element_type_e
 
     /// <summary>string element type</summary>
     STRING = 24,
+
+    /// <summary>f4e2m1 element type</summary>
+    F4E2M1 = 25,
+
+    /// <summary>f8e8m0 element type</summary>
+    F8E8M0 = 26,
 }
 
 
